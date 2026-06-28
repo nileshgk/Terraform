e0e1597d3bbdff6f2e4fba2fd8fb2548bdcee58d
nileshgk <nileshgk774@gmail.com>
2026-06-29 02:08:00 +0530

    Added user data to install nginx and echo massage

--- EC2instance.tf
+++ EC2instance.tf
@@ -5,4 +5,14 @@ resource "aws_instance" "MyEC2Instance" {
   tags = {
     Name = "my-ec2-instance"
   }
+# Injecting the User Data script
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y nginx
+              systemctl start nginx
+              systemctl enable nginx
+              echo "<h1>Successfully deployed Nginx via Terraform!</h1>" > /usr/share/nginx/html/index.html
+              EOF
+
 }
