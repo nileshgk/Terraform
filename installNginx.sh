564778c2dae60bd02c3ef849cbafd5d4342cd35a
nileshgk <nileshgk774@gmail.com>
2026-07-30 01:36:05 +0530

    modified the mini project files

--- installNginx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-yum update -y
-yum install -y nginx
-systemctl start nginx
-systemctl enable nginx
-echo "<h1>Successfully deployed Nginx via Terraform!</h1>" > /usr/share/nginx/html/index.html
--- var.tf
+++
@@ -1,8 +0,0 @@
-variable"AWS_ACCESS_KEY" {}
-variable "AWS_SECRET_KEY" {}
-variable "AWS_REGION" {
-    default = "us-east-1"
-}
-variable "PATH_TO_PUBLIC_KEY" {
-    default = "my_local_key.pub"
-}
